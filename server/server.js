b54bd184489d6e79f15a1fb511f55f348feaf1d7
Vivek Gangdhar <vivekgangdhar11@gmail.com>
2025-12-22 23:32:18 +0530

    first commit

---
+++ server/server.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Resume Analyzer Backend Running");
+});
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
